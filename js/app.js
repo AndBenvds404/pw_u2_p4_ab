d5aa55ac665701ec686cf9f3ab57814ecc1d4972
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-08 19:43:08 -0500

    t14 Vue directivas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,43 @@
 console.log("componente Vue")
 console.log(Vue)
-
+var check = true;
 const app= Vue.createApp({
-    template:`
+   /* template:`
     <h1> HOLA Mundo </h1>
-    <p> desde Vue.JS</p>
-    `
+    <p> desde Vue.JS</p> 
+    <p> {{2+2}}</p> 
+    `*/
+    data(){  //permite vincular informacion de la vista con mi programacion Js
+        return {
+            nombre:'Andres',
+            apellido: 'Benavides',
+            mensaje:'hola mundo desde VueJS T_T'
+        }
+    },
+
+    //opcion de metodos
+    methods:{
+        cambiarNombre(){
+            console.log('boton cambiar nombre')
+            console.log(this.nombre)
+            if (check){
+                this.nombre='Vegeta' 
+                check=false
+            }else{
+                this.nombre='Andres'
+                check=true
+            }
+        }
+        ,
+
+        cambiarApellido(){
+            console.log('boton cambiar apellido')
+            this.apellido='Cabrera'
+            
+        }
+        
+    }
+   
 })
 
 app.mount('#myIdentinficador')
