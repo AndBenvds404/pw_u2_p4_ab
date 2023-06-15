f29c2a25096bcfbfe477330d1f55e405e951fbf4
AndBenvds404 <andres_benavides17@hotmail.com>
2023-06-15 13:30:25 -0500

    Tarea7 calculadora en Vue

--- js/calculadora.js
+++ js/calculadora.js
@@ -7,50 +7,59 @@ var numero2 = 0;
 
 const app = Vue.createApp({
 
+    data(){
+        return{
+            res : resultado
+        }
+    },
+
     methods:{
 
-         sumar (num1, num2) {num1+num2},
-         restar (num1, num2){ num1-num2},
-         multiplicar (num1, num2){ num1*num2},
-         dividir (num1, num2){ num1/num2},
+         sumar (num1, num2) {return num1+num2},
+         restar (num1, num2){ return num1-num2},
+         multiplicar (num1, num2){ return num1*num2},
+         dividir (num1, num2){ return num1/num2},
 
         insertarElemento(num){
+            
             var contenido = num;
             expresion += contenido;
-            console.log("contenido: "+ expresion);
-
-        },
+            this.res=expresion;
+           },
 
         calcular(){
 
             numero2 = parseInt(expresion);
-            
+            console.log("calcular"+numero2+"  "+numero1 + "operador: "+operador)
             switch (operador){
                 case '+':
-                    resultado = sumar(numero1,numero2);
+                   
+                    this.res ="=  "+ this.sumar(numero1,numero2);
                     break;
                 case '-':
-                    '= '+restar(numero1,numero2);
+                    this.res = "=  "+this.restar(numero1,numero2);
                    
                     break;
                 case '*':
-                    '= '+multiplicar(numero1,numero2);
+                    this.res = "=  "+this.multiplicar(numero1,numero2);
                   
                     break;
-                case '/':
-                    '= '+dividir(numero1,numero2).toFixed(5);
+                case '÷':
+                    this.res = "=  "+ this.dividir(numero1,numero2).toFixed(4);
                     
                     break;
         
             }
-            concatenarNum = "";
+            expresion = " ";
+            
         
         },
 
         
         operacion(signo){
             numero1 = parseInt(expresion);
-             operador = signo;
+            operador = signo;
+            this.res= signo+"  " +numero1 
             expresion = "";    
             
         }
